c623202d30f258fe79709f0c93a1bf2575d5670b
josyelealencar <104744522+josyelealencar@users.noreply.github.com>
2023-07-02 19:31:44 -0300

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,26 +1,45 @@
 public class Admin extends Cliente {
 
-    public Admin(String nome, String cpf, int idade) {
+   public Admin(String nome, String cpf, int idade) {
         super(nome, cpf, idade);
     }
 
-    public void inserirProduto(Produto produto) {
-        // Implemente a lógica de inserção de produto
+        public void inserirProduto(Produto produto) {
+            produtos.add(produto);
+        }
+    
+        public void alterarProduto(Produto produto) {
+            for (int i = 0; i < produtos.size(); i++) {
+                if (produtos.get(i).getNome().equals(produto.getNome())) {
+                    produtos.set(i, produto);
+                    break;
+                }
+            }
+        }
+    
+        public void removerProduto(Produto produto) {
+            produtos.remove(produto);
+        }
+    
+        public void atualizarEstoque(Produto produto, int quantidade) {
+            for (int i = 0; i < produtos.size(); i++) {
+                if (produtos.get(i).getNome().equals(produto.getNome())) {
+                    Produto produtoExistente = produtos.get(i);
+                    produtoExistente.setQuantidadeEstoque(produtoExistente.getQuantidadeEstoque() + quantidade);
+                    break;
+                }
+            }
+        }
+    
+        public void exibirRelatorio() {
+            System.out.println("Relatório de Produtos em Falta no Estoque:");
+            for (Produto produto : produtos) {
+                if (produto.getQuantidadeEstoque() == 0) {
+                    System.out.println("Nome: " + produto.getNome());
+                    System.out.println("Categoria: " + produto.getCategoria());
+                    System.out.println("---------------------------");
+                }
+            }
+        }
     }
-
-    public void alterarProduto(Produto produto) {
-        // Implemente a lógica de alteração de produto
-    }
-
-    public void removerProduto(Produto produto) {
-        // Implemente a lógica de remoção de produto
-    }
-
-    public void atualizarEstoque(Produto produto, int quantidade) {
-        // Implemente a lógica de atualização do estoque
-    }
-
-    public void exibirRelatorio() {
-        // Implemente a lógica para exibir relatórios
-    }
-}
+    
